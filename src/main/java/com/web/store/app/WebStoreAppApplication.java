cc99ccb46ea20d14b8cd1713bbe64042ff90e9b3
Grgin <grginivo@gmail.com>
2023-03-05 18:15:03 +0100

    created project

---
+++ src/main/java/com/web/store/app/WebStoreAppApplication.java
@@ -0,0 +1,13 @@
+package com.web.store.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreAppApplication.class, args);
+    }
+
+}
