fd740e76aec9f97b0cbb2e6ec166967e16ebfce8
igrgin <grginivo@gmail.com>
2023-06-01 22:38:23 +0200

    fix nested field

--- src/main/java/com/web/store/app/backend/transaction/dto/CartProduct.java
+++ src/main/java/com/web/store/app/backend/transaction/dto/CartProduct.java
@@ -1,4 +1,8 @@
 package com.web.store.app.backend.transaction.dto;
 
-public record CartProduct(String id, Integer quantity, String name, Float price) {
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+public record CartProduct(@Field(type = FieldType.Keyword) String id, @Field(type = FieldType.Integer) Integer quantity,
+                          @Field(type = FieldType.Keyword) String name, @Field(type = FieldType.Float) Float price) {
 }
